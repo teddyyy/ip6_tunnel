a5f82e336f93a721aa455da4f08b1c19cdb5e6b1
KIMOTO Mizuki <teddy@sfc.wide.ad.jp>
2018-01-11 08:39:23 +0900

    remove unused variable

--- scripts/setup_ip6tunnel_skinny.sh
+++ scripts/setup_ip6tunnel_skinny.sh
@@ -6,12 +6,11 @@ NETNS_TUN_NAME="ip6tnl1"
 NETNS_NAME="skinny-ns"
 
 # network
-UNDERLAY_NETWORK6="2001:2:0:1::/64"
+UNDERLAY_NET6="2001:2:0:1::/64"
 UNDERLAY_LOCAL_IP6="2001:2:0:1::1/64"
 UNDERLAY_REMOTE_IP6="2001:2:0:1::2/64"
-OVERLAY_NETWORK6="2001:3:0:1::/64"
+OVERLAY_NET6="2001:4:0:1::/64"
 OVERLAY_LOCAL_IP6="2001:3:0:1::1/64"
-OVERLAY_REMOTE_IP6="2001:3:0:1::2/64"
 
 # path
 KERNEL_MODULE_PATH="../kmod"
@@ -27,7 +26,7 @@ function setup_netns {
 	ip link set ${NETNS_DEV_NAME} netns ${NETNS_NAME}
 	ip netns exec ${NETNS_NAME} ip link set ${NETNS_DEV_NAME} up
 	ip netns exec ${NETNS_NAME} ip -6 addr add ${UNDERLAY_LOCAL_IP6} dev ${NETNS_DEV_NAME}
-	ip netns exec ${NETNS_NAME} ip -6 route add ${UNDERLAY_NETWORK6} dev ${NETNS_DEV_NAME}
+	ip netns exec ${NETNS_NAME} ip -6 route add ${UNDERLAY_NET6} dev ${NETNS_DEV_NAME}
 }
 
 function setup_kernelmodule {
@@ -53,7 +52,7 @@ function setup_ip6tunnel {
 	ip netns exec ${NETNS_NAME} ${CUSTOM_IP_PATH} -6 tunnel add ${NETNS_TUN_NAME} mode skinny remote ${UNDERLAY_REMOTE_IP6} local ${UNDERLAY_LOCAL_IP6} dev ${NETNS_DEV_NAME}
 	ip netns exec ${NETNS_NAME} ip link set dev ${NETNS_TUN_NAME} up
 	ip netns exec ${NETNS_NAME} ip -6 addr add ${OVERLAY_LOCAL_IP6} dev ${NETNS_TUN_NAME}
-	ip netns exec ${NETNS_NAME} ip -6 route add ${OVERLAY_NETWORK6} dev ${NETNS_TUN_NAME}
+	ip netns exec ${NETNS_NAME} ip -6 route add ${OVERLAY_NET6} dev ${NETNS_TUN_NAME}
 }
 
 if [ ${EUID:-${UID}} -ne 0 ]; then
